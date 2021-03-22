6e9e4c2af366b8c0c00c0da4889318ebacc834c6
Primož Mav <primoz.mav@gmail.com>
2021-03-22 22:10:12 +0100

    BMI formating

--- BMI.py
+++ BMI.py
@@ -1,16 +1,18 @@
 print("Vnesi svojo težo v kilogramih:")
-mass=int(input())
+mass = int(input())
 print(mass)
 
 print("Vnesi svojo višino v centimetrih:")
-height=int(input())
+height = int(input())
 print(height)
 
-def calc_BMI(height,mass):
-	BMI=mass/((height/100)**2)
-	return BMI
+
+def calc_BMI(height, mass):
+    BMI = mass/((height/100)**2)
+    return BMI
+
 
 print(f"Tvoj BMI je: {calc_BMI(height,mass):.2f}")
 
-print("Press enter to continue.")
+print("Press enter to continue...")
 input()
